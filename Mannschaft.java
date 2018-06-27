02163990e2aa896bac3c6fd055668e1a2b54795d
Joshua <joshwol12@yahoo.de>
2018-06-27 11:08:15 +0200

    Mannschaft

--- Mannschaft.java
+++ Mannschaft.java
@@ -9,41 +9,25 @@ public class Mannschaft
 {
     // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
     private String name;
-    private int punkte;
-    private int tore;
+    
 
     /**
      * Konstruktor für Objekte der Klasse Laender
      */
     public Mannschaft(String name)
     {
-        // Instanzvariable initialisieren
-        punkte=0;
+       
         this.name=name;
     }
 
-    public int gibPunkte()
-    {
-        return punkte;
-    }
+    
     
     public String gibName()
     {
        return name;
     }
     
-    public int gibTore()
-    {
-        return tore;
-    }
+   
     
-    public void addPunkte(int punkte)
-    {
-        this.punkte += punkte;
-    }
     
-    public void addTore(int tore)
-    {
-        this.tore+=tore;
-    }
 }
